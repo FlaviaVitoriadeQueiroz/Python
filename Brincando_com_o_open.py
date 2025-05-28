a295204d2435c8d3c3d425d33da1c9f1dbe9dff8
Flávia Vitória de Queiroz <flaviavitoriaqueiroz@gmail.com>
2025-05-28 18:10:01 -0300

    Add files via upload

---
+++ Brincando_com_o_open.py
@@ -0,0 +1,5 @@
+arquivo = open('open.csv', 'w', encoding = 'utf-8')
+arquivo.write('Nome: Flávia, Idade:19, Cidade: Colina\n')
+arquivo.write('Nome: Alfredo, Idade: 21, Cidade:Barretos\n')
+
+arquivo.close()
