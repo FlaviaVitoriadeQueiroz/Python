af6f431c3dee7516d17e513e96d15b67f066eeae
Flávia Vitória de Queiroz <flaviavitoriaqueiroz@gmail.com>
2025-05-31 21:53:10 -0300

    Add files via upload

--- técnicascomplementaresparaloops.py
+++ técnicascomplementaresparaloops.py
@@ -21,3 +21,20 @@ print(lista)
 
 lista = [i*3 for i in range(9, 18)]
 print(lista)
+
+#é diferente para tuplas
+tupla = tuple(i for i in range(9, 18))
+print(tupla)
+
+#é diferente para dicionários
+dicionario = {i: i for i in range(9,180)}
+print(dicionario)
+
+#é diferente para conjuntos
+conjuntos = {i**2 for i in range(5)}
+print(conjuntos)
+
+#Lista	[expressão for item in iterável]	[i**2 for i in range(5)]
+#Tupla	tuple(expressão for item in iterável)	tuple(i**2 for i in range(5))
+#Conjunto	{expressão for item in iterável}	{i**2 for i in range(5)}
+#Dicionário	{chave: valor for item in iterável}	{i: i**2 for i in range(5)}
