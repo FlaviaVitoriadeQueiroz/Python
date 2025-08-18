80b0b9376ef9c8e23b5f63024c251da6f3f07e7b
Flávia Vitória de Queiroz <flaviavitoriaqueiroz@gmail.com>
2025-08-18 14:15:15 -0300

    Delete algoritmoras e programação directory

--- algoritmoras e programação/atividade 3 ap.py
+++
@@ -1,8 +0,0 @@
-def calcular_viagem():
-    tempo = int(input('Digite o tempo em horas: '))
-    velocidade = float(input('Digite a velocidade média (km/h): '))
-    distancia = velocidade * tempo
-    quantidade_de_combustivel = distancia / 1 
-    print("A quantidade de combustível utilizada foi de:", quantidade_de_combustivel)
-
-calcular_viagem()
