69f93b8aa2b9f9b957f0f6c1be847119802abb80
Flávia Vitória de Queiroz <flaviavitoriaqueiroz@gmail.com>
2025-08-18 14:17:47 -0300

    Delete prova 4 ap.py

--- prova 4 ap.py
+++
@@ -1,9 +0,0 @@
-n = int(input()) # Lê um número inteiro fornecido pelo usuário
-a, b = 1, 1 # Inicializa os dois primeiros termos da sequência de Fibonacci
-contador = 0 # Inicializa o contador que armazenará a quantidade de termos da sequência menores ou iguais a 'n'
-while a <= n: # Executa o laço enquanto o termo atual 'a' for menor ou igual a 'n'
-    contador += 1 # Incrementa o contador
-    a, b = b, a + b # Atualiza os dois termos da sequência de Fibonacci
-
-print(contador) # Exibe o total de termos da sequência de Fibonacci que são menores ou iguais a 'n'
-
