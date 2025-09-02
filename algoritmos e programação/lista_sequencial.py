5efde4931c3caa45483a3ec58073def1bbf6f098
Flávia Vitória de Queiroz <flaviavitoriaqueiroz@gmail.com>
2025-09-02 19:35:18 -0300

    Add files via upload

---
+++ algoritmos e programação/lista_sequencial.py
@@ -0,0 +1,45 @@
+class ListaSquencial:
+    def __init__ (self, capacidade): #construtor 
+        self.capacidade = capacidade #a capacidade e fixa por ser uma lista sequencial 
+        self.dados = [None] * capacidade #array fixo, que vai criar um numero x espacos vazios na memoria de acordo com o valor dado em capacidade
+        self.tamanho = 0 #quantidade atual de elementos
+    
+    def inserir(self, elemento):
+        if self.tamanho < self.capacidade:
+            self.dados[self.tamanho] = elemento #o novo elemento é armazenado na posição atual da lista (self.dados), no índice self.tamanho
+            self.tamanho += 1 #Depois de inserir o elemento, aumenta o contador tamanho, já que agora há mais um elemento na lista
+        else:
+            print("Lista cheia")
+    
+    def remover (self, indice):
+        if 0 <= indice < self.tamanho: # Tem que ser menor que self.tamanho (ou seja, dentro da faixa de elementos válidos)
+            for i in range(indice, self.tamanho -1): #Esse laço desloca os elementos à direita para a esquerda, a partir do índice onde o elemento foi removido
+                self.dados[i] = self.dados[i + 1] #Move o elemento da posição seguinte (i + 1) para a posição atual (i), preenchendo o espaço deixado pela remoção
+            self.dados[self.tamanho - 1] = None #Depois de deslocar os elementos, o último elemento da lista agora está duplicado e precisa ser apagado
+            self.tamanho -= 1 #Diminui o contador de tamanho, ja que um elemento foi removido
+        else:
+            print("Indice invalido")
+    
+    def buscar (self, elemento): 
+        for i in range(self.tamanho):
+            if self.dados[i] == elemento: #comparando para saber se e isso que eu quero
+                return i #retorna o indice do elemento encontrado
+        return -1 #Elemento não encontrado
+    
+    def imprimir(self):
+        for i in range(self.tamanho):
+            print(self.dados[i], end ="|")
+        print()
+
+#Usando
+lista = ListaSquencial(18) #criando uma lista com capacidade para 18 elementos
+lista.inserir(3)
+lista.inserir(6)
+lista.inserir(9)
+lista.inserir(12)
+lista.inserir(15)
+lista.inserir(18)
+lista.imprimir()
+print("indice do 18:", lista.buscar(18))
+lista.remover(3) #remove oq esta no indice 3
+lista.imprimir()
