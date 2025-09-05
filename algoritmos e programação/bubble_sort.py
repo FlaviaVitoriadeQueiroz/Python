1fefd568da57f955ab2984663c61bd205bdd888c
Flávia Vitória de Queiroz <flaviavitoriaqueiroz@gmail.com>
2025-09-05 17:11:06 -0300

    Add files via upload

---
+++ algoritmos e programação/bubble_sort.py
@@ -0,0 +1,25 @@
+lista = [ 'h', 'a', 'i','d', 'f', 'k', 'g']
+lista2 = [65, 78, 98, 40, 3, 6, 12, 34, 54, 90, 80]
+
+def bubble_sort(lista):
+    n = len(lista) #identifica o tamanho da lista
+    for i in range(n): #percorre toda a lista
+        for j in range(0, n - i - 1): #compara cada par de elementos adjacentes
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j] #troca se estiver fora de ordem
+
+print("Lista original:", lista)
+bubble_sort(lista)
+print("Lista ordenada:", lista)
+
+print("Lista original:", lista2)
+bubble_sort(lista2)
+print("Lista ordenada:", lista2)
+
+'''E um algoritmo simples
+um dos metodos mais classicos
+
+tempo de execucao maior 0(n^2), por causa dos dois loops aninhados
+pouco eficiente em listas grandes
+
+e um elemento estavel, ou seja mantem a ordem dos elementos iguais'''
